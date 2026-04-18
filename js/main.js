9b84e60f1de9c03984b60dada267192965f445d3
Dzulius <ayobamitemidayo311@gmail.com>
2026-04-18 17:21:06 +0100

    Hero slideshow complete

--- js/main.js
+++ js/main.js
@@ -3,3 +3,15 @@ const navToggle = document.querySelector('.nav-toggle')
 navToggle.addEventListener('click', function() {
     navLinks.classList.toggle('open')
 })
+
+const slides = document.querySelectorAll('.slide');
+let currentIndex = 0;
+
+setInterval(function() {
+    slides[currentIndex].classList.remove('active');
+    currentIndex = currentIndex + 1;
+    if (currentIndex >= slides.length) {
+        currentIndex = 0;
+    }
+    slides[currentIndex].classList.add('active');
+}, 5000);
